fe25737547b2dea6592befb6737896ac41a578d7
tonygallen <25624821+tonygallen@users.noreply.github.com>
2025-12-18 14:27:54 +0000

    Deploying to gh-pages from @ tonygallen/tonygallen.github.io@66876b3941388e737c6b14ef5477079a8b9b7371 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "tonygallen/tonyallen.github.io",
+    "data-repo": "tonygallen/tonygallen.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
